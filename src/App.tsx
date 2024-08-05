d234c9a960c1d3fe80a045b46a52090512e083c5
Tyler Goelz <tyler@tylergoelz.com>
2024-08-05 04:14:24 -0700

    update recipe display (post search)

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,11 @@ import CardContent from "@mui/material/CardContent";
 import Checkbox from "@mui/material/Checkbox";
 import Chip from "@mui/material/Chip";
 import Container from "@mui/material/Container";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 import FormControl from "@mui/material/FormControl";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormGroup from "@mui/material/FormGroup";
@@ -47,27 +52,16 @@ const GENERATE_RECIPES = gql`
 const App: React.FC = () => {
   const [ingredients, setIngredients] = useState<string[]>([]);
   const [preferences, setPreferences] = useState<string[]>([]);
+  const [showDialog, setShowDialog] = useState<boolean>(false);
 
   const [generateRecipes, { loading, data }] = useMutation(GENERATE_RECIPES);
 
-  const handleGenerateRecipes = async () => {
-    /**
-     * TODO:
-     * Add validation that ensures the user has put in more than two ingredients
-     */
-    /**
-     * TODO:
-     * Adjust this auto-generated pattern to destructure loading, data,
-     * and error from useMutation hook instead of using additional react state
-     */
-    console.log({
-      variables: {
-        input: {
-          ingredients,
-          preferences,
-        },
-      },
-    });
+  function handleGenerateRecipes() {
+    if (ingredients.length < 3) {
+      setShowDialog(true);
+      return;
+    }
+
     generateRecipes({
       variables: {
         input: {
@@ -76,127 +70,177 @@ const App: React.FC = () => {
         },
       },
     });
-  };
+  }
 
   return (
-    <Container>
-      <Header />
-      <Container maxWidth="md" sx={{ mt: 6 }}>
-        <Typography align="center" variant="h3" component="h1" gutterBottom>
-          Ingredient Inspired Recipes
-        </Typography>
-        <Typography align="center" variant="h5" component="h2">
-          Simply add ingredients and MealMate's AI will generate recipes just
-          for you.
-        </Typography>
-        <Box
-          sx={{
-            display: "flex",
-            mt: 4,
-          }}
-        >
-          <Autocomplete
-            multiple
-            fullWidth
-            freeSolo
-            options={[]}
-            value={ingredients}
-            onChange={(event, newValue) => {
-              setIngredients(newValue);
+    <>
+      <Container>
+        <Header />
+        <Container maxWidth="md" sx={{ mt: 6 }}>
+          <Typography align="center" variant="h3" component="h1" gutterBottom>
+            Ingredient Inspired Recipes
+          </Typography>
+          <Typography align="center" variant="h5" component="h2">
+            Simply add ingredients and MealMate's AI will generate recipes just
+            for you.
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              mt: 4,
             }}
-            renderTags={(value: string[], getTagProps) => {
-              return value.map((option: string, index: number) => {
-                return <Chip label={option} {...getTagProps({ index })} />;
-              });
-            }}
-            renderInput={(params) => {
-              return (
-                <TextField
-                  {...params}
-                  variant="outlined"
-                  label="Add Ingredient"
-                  placeholder="Type an ingredient and press Enter"
-                />
-              );
-            }}
-          />
-          <Button
-            disabled={loading}
-            variant="contained"
-            color="primary"
-            size="large"
-            onClick={handleGenerateRecipes}
-            sx={{ ml: 1, width: "35%" }}
           >
-            Generate Recipes
-          </Button>
-        </Box>
-        <Box sx={{ mt: 2, display: "flex", alignItems: "center" }}>
-          <FormControl>
-            <FormLabel>
-              <Typography variant="caption" display="block" sx={{ pr: 1 }}>
-                Dietary Preferences:
-              </Typography>
-            </FormLabel>
-            <FormGroup row>
-              {[
-                "Vegan",
-                "Vegetarian",
-                "Gluten Free",
-                "Dairy Free",
-                "Nut Free",
-              ].map((preference) => {
+            <Autocomplete
+              multiple
+              fullWidth
+              freeSolo
+              options={[]}
+              disabled={loading}
+              value={ingredients}
+              onChange={(event, newValue) => {
+                setIngredients(newValue);
+              }}
+              renderTags={(value: string[], getTagProps) => {
+                return value.map((option: string, index: number) => {
+                  return <Chip label={option} {...getTagProps({ index })} />;
+                });
+              }}
+              renderInput={(params) => {
                 return (
-                  <FormControlLabel
-                    disabled={loading}
-                    control={
-                      <Checkbox
-                        onChange={(e) =>
-                          setPreferences((prev) => {
-                            if (e.target.checked) {
-                              return [...prev, preference];
-                            }
-
-                            return prev.filter((selectedPreference) => {
-                              return selectedPreference !== preference;
-                            });
-                          })
-                        }
-                      />
-                    }
-                    label={preference}
+                  <TextField
+                    {...params}
+                    variant="outlined"
+                    label="Add Ingredient"
+                    placeholder="Type an ingredient and press Enter"
                   />
                 );
-              })}
-            </FormGroup>
-          </FormControl>
-        </Box>
-        <Grid container spacing={2} sx={{ mt: 2 }}>
-          {data?.generateRecipes?.edges?.map(
-            ({ node: recipe }: { node: Recipe }) => (
-              <Grid item xs={12} sm={6} md={4} key={recipe.id}>
-                <Card>
-                  <CardContent>
-                    <Typography variant="h3" gutterBottom>
-                      {recipe.title}
-                    </Typography>
-                    <Typography variant="body2" paragraph>
-                      {recipe.description}
-                    </Typography>
-                    <Typography variant="body2" paragraph>
-                      Ingredients: {recipe.ingredients.join(", ")}
-                    </Typography>
-                    <Typography variant="body2" paragraph>
-                      Instructions: {recipe.instructions.join(", ")}
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-            )
-          )}
-        </Grid>
+              }}
+            />
+            <Button
+              disabled={loading}
+              variant="contained"
+              color="primary"
+              size="large"
+              onClick={handleGenerateRecipes}
+              sx={{ ml: 1, width: "35%" }}
+            >
+              Generate Recipes
+            </Button>
+          </Box>
+          <Box sx={{ mt: 2, display: "flex", alignItems: "center" }}>
+            <FormControl>
+              <FormLabel>
+                <Typography variant="caption" display="block" sx={{ pr: 1 }}>
+                  Dietary Preferences:
+                </Typography>
+              </FormLabel>
+              <FormGroup row>
+                {[
+                  "Vegan",
+                  "Vegetarian",
+                  "Gluten Free",
+                  "Dairy Free",
+                  "Nut Free",
+                ].map((preference) => {
+                  return (
+                    <FormControlLabel
+                      disabled={loading}
+                      control={
+                        <Checkbox
+                          onChange={(e) =>
+                            setPreferences((prev) => {
+                              if (e.target.checked) {
+                                return [...prev, preference];
+                              }
+
+                              return prev.filter((selectedPreference) => {
+                                return selectedPreference !== preference;
+                              });
+                            })
+                          }
+                        />
+                      }
+                      label={preference}
+                    />
+                  );
+                })}
+              </FormGroup>
+            </FormControl>
+          </Box>
+          <Grid container spacing={2} sx={{ mt: 2 }}>
+            {data?.generateRecipes?.edges?.map(
+              ({ node: recipe }: { node: Recipe }) => (
+                <Grid item xs={12} sm={6} md={12} key={recipe.id}>
+                  <Card>
+                    <CardContent>
+                      <Typography variant="h4" gutterBottom>
+                        {recipe.title}
+                      </Typography>
+                      <Typography variant="subtitle1" paragraph>
+                        {recipe.description}
+                      </Typography>
+                      <Typography variant="h5">Ingredients:</Typography>
+                      <ul>
+                        {recipe.ingredients.map((ingredient, index) => (
+                          <li key={index}>
+                            <Typography>{ingredient}</Typography>
+                          </li>
+                        ))}
+                      </ul>
+                      <Typography variant="h5" paragraph>
+                        Instructions:
+                      </Typography>
+                      <ol>
+                        {recipe.instructions.map((instruction, index) => (
+                          <li key={index}>
+                            <Typography paragraph>{instruction}</Typography>
+                          </li>
+                        ))}
+                      </ol>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              )
+            )}
+          </Grid>
+        </Container>
       </Container>
-    </Container>
+      {showDialog ? (
+        <Dialog
+          maxWidth="sm"
+          open
+          keepMounted
+          onClose={(e) => {
+            setShowDialog(false);
+          }}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            Get the most from MealMate's AI
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              <Typography paragraph>
+                Please add at least 3 ingredients. If you're having trouble
+                coming up with ingredients, try adding ingredients you already
+                have in your kitchen.
+              </Typography>
+            </DialogContentText>
+            <DialogActions>
+              <Button
+                variant="outlined"
+                onClick={(e) => {
+                  setShowDialog(false);
+                }}
+              >
+                Close
+              </Button>
+            </DialogActions>
+          </DialogContent>
+        </Dialog>
+      ) : null}
+    </>
   );
 };
 
